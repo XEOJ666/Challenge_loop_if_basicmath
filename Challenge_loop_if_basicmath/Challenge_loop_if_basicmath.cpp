b4be121c7708ea9e2b7f94203187ad9c63351917
Jirayut Senanorit <araeize_468_arae@hotmail.com>
2020-08-13 21:49:48 +0700

    add Error

--- Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
+++ Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
@@ -5,7 +5,9 @@ int main()
     double	 i, b, x = 0, xsqr = 0, c, d, e;
     scanf_s("%lf %lf", &i, &b);
     int n = abs(i - b) + 1;
-    if (i <= b)
+    if (i == b)
+        printf("Error");
+    else if (i <= b)
     {
         for (i = i;i <= b;i++)
         {
