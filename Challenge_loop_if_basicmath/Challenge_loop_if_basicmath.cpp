ff2e35fde02b8df911320b19bc2ac8712b19925e
Jirayut Senanorit <araeize_468_arae@hotmail.com>
2020-08-13 21:42:54 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Challenge_loop_if_basicmath.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<cmath>
+int main()
+{
+    double	 i, b, x = 0, xsqr = 0, c, d, e;
+    scanf_s("%lf %lf", &i, &b);
+    int n = abs(i - b) + 1;
+    if (i <= b)
+    {
+        for (i = i;i <= b;i++)
+        {
+            printf("%.f ", i);
+            x += i;
+            xsqr += pow(i, 2);
+        }
+        c = (n * xsqr - pow(x, 2));
+        d = (n * (n - 1));
+        e = sqrt(c / d);
+        puts(" ");
+        printf("Average = %.2f\n", x / n);
+        printf("SD = %.2f", (e));
+    }
+    else if (i >= b)
+    {
+        for (i = i;i >= b;i--)
+        {
+            printf("%.f ", i);
+            x += i;
+            xsqr += pow(i, 2);
+        }
+        c = (n * xsqr - pow(x, 2));
+        d = (n * (n - 1));
+        e = sqrt(c / d);
+        puts(" ");
+        printf("Average = %.2f\n", x / n);
+        printf("SD = %.2f", (e));
+    }
+}
